5c59ec00b361069427570bcaa81456e422424e39
taeyun <lwe12f675@gmail.com>
2024-09-04 21:45:08 +0900

    feat: 앱 라우터의 데이터 패칭

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,21 +1,55 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
 import books from "@/mock/books.json";
+import { BookData } from "@/types";
+
+const AllBooks = async () => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`
+  );
+  // 예외처리
+  if (!response.ok) return <div>오류가 발생했습니다...</div>;
+
+  const allBooks: BookData[] = await response.json();
+  console.log(allBooks);
+
+  return (
+    <div>
+      {allBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+};
+
+const RecoBooks = async () => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`
+  );
+  if (!response.ok) return <div>오류가 발생했습니다...</div>;
+
+  const recoBooks: BookData[] = await response.json();
+  console.log(recoBooks);
+
+  return (
+    <div>
+      {recoBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+};
 
 export default function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <RecoBooks />
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <AllBooks />
       </section>
     </div>
   );
