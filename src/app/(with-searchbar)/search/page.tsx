0a9f371259722a38a9345f3ceb49e3413726b7ed
taeyun <lwe12f675@gmail.com>
2024-09-04 18:06:46 +0900

    feat: UI 구현

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,17 +1,18 @@
-//* 쿼리스트링을 꺼내 쓸때는 페이지컴포넌트의 props로 꺼내쓰면 된다.
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
-import ClientComponent from "@/components/client-component";
-
-//* AppRouter에서 쿼리스트링이나 URL파라미터처럼 경로와 함께 명시되는 값들은 페이지 컴포넌트의 props로 다 전달이 된다.
-const Page = ({ searchParams }: { searchParams: { q?: string } }) => {
+export default function Page({
+  searchParams,
+}: {
+  searchParams: {
+    q?: string;
+  };
+}) {
   return (
     <div>
-      검색 페이지 {searchParams.q}
-      <ClientComponent>
-        <></>
-      </ClientComponent>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
     </div>
   );
-};
-
-export default Page;
+}
--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -1,28 +1,39 @@
 "use client";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
 
-//* 브라우저에서 하이드레이션 되도록 use client 선언
-//* 입력이나 클릭같은 상호작용이 있으면 그런 컴포넌트들만 떼서 클라이언트 컴포넌트로 만들어주기
-//? 중요한점은 앱라우터에서 페이지의 데이터가 서버 컴포넌트는 RSC페이로드로 클라이언트 컴포넌트는 자바스크립트 번들로 나뒤어서 동시에 전달이 된다.
-const Searchbar = () => {
-  const [search, setSearch] = useState("");
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import style from "./serachbar.module.css";
+
+export default function Searchbar() {
   const router = useRouter();
+  const searchParams = useSearchParams(); //* 앱라우터에서는 useSearchParams() 훅을 써야 쿼리스트링을 꺼낼 수 있음
+  const [search, setSearch] = useState("");
+
+  const q = searchParams.get("q");
+
+  useEffect(() => {
+    setSearch(q || "");
+  }, [q]);
 
   const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
 
   const onSubmit = () => {
+    if (!search || q === search) return;
     router.push(`/search?q=${search}`);
   };
 
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
+
   return (
-    <div>
-      <input type="text" value={search} onChange={onChangeSearch} />
+    <div className={style.container}>
+      <input value={search} onChange={onChangeSearch} onKeyDown={onKeyDown} />
       <button onClick={onSubmit}>검색</button>
     </div>
   );
-};
-
-export default Searchbar;
+}
