87dab3ab9568462d9154abdc10d123c08750ecef
taeyun <lwe12f675@gmail.com>
2024-09-05 20:09:26 +0900

    feat: 풀 라우트 캐시 (동적 경로에 적용하기)

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,6 +1,10 @@
 import BookItem from "@/components/book-item";
 import { BookData } from "@/types";
 
+//* 검색 페이지는 searchParams라는 쿼리스트링을 보관하는 이러한 Props를 받고 있다. 동적함수를 사용하고 있기때문에 다이나믹 페이지로 설정돼있다.
+//* 스태틱 페이지로 설정하려면 동적함수를 사용하지 못하도록 해야하는데 그러면 쿼리스트링을 사용하지 못하니 그렇게 할 수는 없다.
+//* 이렇게 검색페이지 처럼 실시간으로 사용자의 검색어를 기반으로 어떠한 데이터를 백엔드 서버에서 부터 불러와 렌더링해줘야 하는 페이지는 스태틱으로 설정할 수 없다.
+//* 그래도 조금이라도 설정하고 싶다면, 데이터 캐시를 활용하여 설정하면 된다.
 export default async function Page({
   searchParams,
 }: {
@@ -9,7 +13,10 @@ export default async function Page({
   };
 }) {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`,
+    {
+      cache: "force-cache", //* 이러면 한번 검색된 페이지는 조금더 빠르게 페이지를 응답해 줄 수 있다.
+    }
   );
 
   if (!response.ok) return <div>오류가 발생했습니다...</div>;
--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -1,6 +1,21 @@
+import { notFound } from "next/navigation";
 import style from "./page.module.css";
 import { BookData } from "@/types";
 
+//? dynamicParams를 false로 설정하면 아래 설정한 generateStaticParams()에 명시하지 않는 id파라미터들은 다 404페이지로 리다이렉트 된다.
+export const dynamicParams = false; // 기본값은 true
+
+//* bool/1, bool/2, bool/3 페이지들은 스태틱 페이지로써 빌드타임에 만들게 된다. (페이지 라우터의 getStaticPaths()와 같은 역할을 한다.)
+//* 1, 2, 3번의 id도서는 풀 라우터 캐시에 보관이 되어 굉장히 빠른 속도로 스태틱 페이지가 반환됨
+//! 주의할점
+//! 1. id는 문자열로만
+//! 2. generateStaticParams()를 내보내주게되면 페이지 컴포넌트 내부에 데이터 캐싱이 설정되지 않은 데이터 패칭이 존재하게 되면 무조건 해당하는 페이지가 스태틱 페이지로써 강제로 설정이 된다.
+export const generateStaticParams = async () => {
+  return [{ id: "1" }, { id: "2" }, { id: "3" }];
+};
+
+//* id의 url파라미터를 받는 동적경로에 대응하는 페이지는 다이나믹 페이지로 설정된다.
+//* 어떠한 id의 파라미터가 들어올 지 모르기 때문에 기본값으론 다이나믹 페이지로 설정된다.
 export default async function Page({
   params,
 }: {
@@ -10,7 +25,12 @@ export default async function Page({
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${params.id}`
   );
 
-  if (!response.ok) return <div>오류가 발생했습니다...</div>;
+  if (!response.ok) {
+    if (response.status === 404) {
+      notFound(); // 없는 페이지일 경우 자동으로 404페이지로 리다이렉트 됨
+    }
+    return <div>오류가 발생했습니다...</div>;
+  }
 
   const book: BookData = await response.json();
 
